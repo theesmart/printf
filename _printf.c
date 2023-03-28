ad6bf751e706fbb5d9411696272d542e9c16408b
Chantal <chantalwilkister@gmail.com>
2023-03-28 05:56:43 -0700

    0

--- _printf.c
+++ _printf.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include"main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - print function
+ * @format: format
+ * Return: printed chars
  */
 int _printf(const char *format, ...)
 {
@@ -36,15 +36,14 @@ int _printf(const char *format, ...)
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
-			++i;
+			i++;
 			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
 	}
-
 	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
@@ -53,9 +52,9 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * print_buffer - prints the contents of the buffer
+ * @buffer: array of chars
+ * @buff_ind: index at which to add next char(length)
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
